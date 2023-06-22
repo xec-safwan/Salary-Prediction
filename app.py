e424edb9a6e90051b406bc9b2275ced86545ce1e
xec-safwan <xec.safwan@gmail.com>
2023-06-23 01:21:11 +0530

    Added interactive and non interactive data based on graph

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+from plotly import graph_objs as go
+import plotly.express as px
 
 st.title("Salary Prediction")
+data = pd.read_csv('./data/Salary_Data.csv')
 
 nav = st.sidebar.radio("",["Home","Prediction","Contribute to Dataset"])
 st.sidebar.image("./data/sal.jpg")
 if nav=="Home":
-    st.write("")
+    if st.checkbox('Show Table'):
+        st.table(data)
+    graph =st.selectbox('Which Type Of Plot?',["Non-InterActive","Interactive"])
 
+    if graph=="Non-InterActive":
+        plt.figure(figsize=(10,5))
+        plt.scatter(data['YearsExperience'],data['Salary'])
+        plt.ylim(0)
+        plt.xlabel("Years of Experience")
+        plt.ylabel("Salary")
+        plt.tight_layout()
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+        st.pyplot()
+    if graph=="Interactive": 
+        fig = px.scatter(data, x="YearsExperience", y="Salary")
+        st.plotly_chart(fig)
 if nav=="Prediction":
     st.write('Prediction')
 
